24a97de5c8ef03b61b8d79d052c3aad8adbd308f
Brycies <brysonodhiambo29@gmail.com>
2023-02-27 17:13:30 +0300

    commit js

---
+++ calculator.js
@@ -0,0 +1,13 @@
+function addToResult(value) {
+  document.getElementById("result").value += value;
+}
+
+function clearResult() {
+  document.getElementById("result").value = "";
+}
+
+function calculateResult() {
+  const result = eval(document.getElementById("result").value);
+  document.getElementById("result").value = result;
+}
+console.log(result);
